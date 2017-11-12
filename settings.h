859671c65a452cd7e5706f8734ef7045a7023d84
Oxy976 <ghost_mail2005-musor@yahoo.com>
2017-11-12 12:20:50 +0300

    actual

---
+++ settings.h
@@ -0,0 +1,32 @@
+// Network settings
+// --
+const char* ssid = "xxx";
+const char* password = "xxx";
+// --
+
+// Time settings
+const char* ntpServerName = "xxx";
+const int TIMEZONE=3;
+
+// narmon
+
+#define SRV   "narodmon.ru"
+#define MAC  "xxx"
+#define PASS  "xxx"
+#define USERNAME  "xxx"
+#define TOPIC  "login/xxx"
+
+//--
+
+//mqtt4narmon
+char server[] = SRV ;
+char authMethod[] = USERNAME;
+char token[] = PASS;
+char clientId[] = MAC;
+char conntopic[] = TOPIC "status";
+
+// LED
+const int LED_BUILTIN=2;  //Onboard LED Pin
+
+
+// Application settings
